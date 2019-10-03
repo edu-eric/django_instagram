2b318eff3222445f1ca218f7285b1d167e54785d
edu-eric <dev.ericlee@gmail.com>
2019-10-03 20:08:39 +0900

    #6. ModelForm을 이용한 게시글 생성 로직 완료

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,16 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
+
+def create(request):
+    if request.method == "POST":
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect("/")
+    else:
+        post_form = PostForm()
+    context = {
+        "post_form": post_form
+    }
+    return render(request, 'posts/create.html', context)
