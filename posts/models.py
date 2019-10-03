c1e7342c61ad393a703ef3eda9ba2537ff6e9078
edu-eric <dev.ericlee@gmail.com>
2019-10-04 00:00:49 +0900

    #38. 좋아요 기능을 위한 M:N 관계 설정

--- posts/models.py
+++ posts/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     content = models.CharField(max_length=100)
     image = models.ImageField(upload_to=image_upload_path(), blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_posts")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
