670185d28c50c0b1af463fdb6d93acf8551cd44d
edu-eric <dev.ericlee@gmail.com>
2019-10-03 23:00:28 +0900

    #22. UserCreationForm을 이용한 회원가입

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        User.objects.create_user(username=username, password=password)
-    return render(request, 'accounts/signup.html')
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('/')
+    else:
+        user_form = UserCreationForm()
+    context = {
+        'user_form': user_form,
+    }
+    return render(request, 'accounts/signup.html', context)
