6dd681974cf7a3d637fd40b3dbbd20103bea96d8
edu-eric <dev.ericlee@gmail.com>
2019-10-03 20:58:28 +0900

    #17. 이미지 파일 출력을 위해 MEDIA 파일 경로 urls.py에 추가해주기

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = "posts"
@@ -9,4 +11,4 @@ urlpatterns = [
     path('<int:post_id>/', views.detail, name="detail"),
     path('<int:post_id>/delete', views.delete, name="delete"),
     path('<int:post_id>/update', views.update, name="update"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
