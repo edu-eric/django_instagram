da25724fd459dbd2aa22fe8f52cbaba6fd25c0a5
edu-eric <dev.ericlee@gmail.com>
2019-10-03 23:12:12 +0900

    #24. AuthenticateForm을 사용한 로그인 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup(request):
@@ -19,10 +19,14 @@ def signup(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        login_form = AuthenticationForm(request, request.POST)
+        if login_form.is_valid():
+			# 검증된 폼에서 유저 객체를 가져오기 위해서는 get_user() 메서드를 사용한다.
+            user = login_form.get_user()
+			# 로그인 함수를 이용해서 검증된 유저에 대해 "세션"을 생성해준다!
             auth_login(request, user)
-            return redirect('/')
-    return render(request, 'accounts/login.html')
+            return redirect("/")
+    context = {
+        'login_form': AuthenticationForm(),
+    }
+    return render(request, 'accounts/login.html', context)
