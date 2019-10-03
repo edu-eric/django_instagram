4e2e62b88d6b2d795e14e393f85d8b15fda8e277
edu-eric <dev.ericlee@gmail.com>
2019-10-03 20:35:25 +0900

    #15. 이미지 업로드 경로 커스터마이징

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+def image_upload_path():
+    return f'article/%Y/%m/%d/'
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=100)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(upload_to=image_upload_path(), blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
