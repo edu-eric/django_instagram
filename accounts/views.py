fc58728d1c32ee5c5ce71a96e2a2af12f7f2e80d
edu-eric <dev.ericlee@gmail.com>
2019-10-03 22:57:21 +0900

    #21. 회원가입 기초 로직 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        User.objects.create_user(username=username, password=password)
+    return render(request, 'accounts/signup.html')
