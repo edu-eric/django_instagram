dca9fc420c656b272589501b1aec2f166566ae15
edu-eric <dev.ericlee@gmail.com>
2019-10-03 23:42:20 +0900

    #35. Comment 모델 생성 완료

--- posts/models.py
+++ posts/models.py
@@ -12,3 +12,10 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
